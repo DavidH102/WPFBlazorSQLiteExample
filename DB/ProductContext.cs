dc318fa71f459a54eeee34008568312f4897cdf3
David Marsh <David@esd.services>
2022-09-13 22:45:18 -0400

    init

---
+++ DB/ProductContext.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using WpfApp1.Model;
+
+namespace WpfApp1.DB
+{
+    public class ProductContext : DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+
+        protected override void OnConfiguring(
+            DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite(
+                "Data Source=products.db");
+            optionsBuilder.UseLazyLoadingProxies();
+        }
+    }
+}
